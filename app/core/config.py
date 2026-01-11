c215976ae7ac3ec24fc48fba641fc7c696bbfc17
Cristian Pinto <cristian.pinto.estay@gmail.com>
2026-01-11 15:02:14 -0300

    modificacion de config.py para desplegar en Render

--- app/core/config.py
+++ app/core/config.py
@@ -3,14 +3,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
+    ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     #env: str = "development"
 
     model_config = SettingsConfigDict(
         env_file=".env",
-        env_file_encoding="utf-8")
+        env_file_encoding="ignore")
 
 
 settings = Settings()

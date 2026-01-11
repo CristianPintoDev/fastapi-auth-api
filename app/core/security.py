c2d035e1372cb508e2eff935ad5eb19ba9f0434a
Cristian Pinto <cristian.pinto.estay@gmail.com>
2026-01-11 09:35:19 -0300

    Bug de contraseña resuelto, incompatibilidad de bcrypt con passlib, la solucion fue instalar bcrypt 4.0.1

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,3 @@
-import hashlib
-import base64
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(
@@ -7,15 +5,14 @@ pwd_context = CryptContext(
     deprecated="auto",
 )
 
+"""
 def _normalize_password(password: str) -> str:
     digest = hashlib.sha256(password.encode("utf-8")).digest()
     return base64.b64encode(digest).decode("utf-8")
-
+"""
+    
 def hash_password(password: str) -> str:
-    return pwd_context.hash(_normalize_password(password))
+    return pwd_context.hash(password)
 
 def verify_password(password: str, password_hash: str) -> bool:
-    return pwd_context.verify(
-        _normalize_password(password),
-        password_hash
-    )
+    return pwd_context.verify(password, password_hash)
